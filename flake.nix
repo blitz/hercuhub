05d5ebf6ddf614aa1c9ea04bfc925a370f0ea65e
Julian Stecklina <julian.stecklina@cyberus-technology.de>
2025-06-04 17:02:18 +0200

    Actually build something in CI

--- flake.nix
+++ flake.nix
@@ -1,15 +1,81 @@
 {
-  description = "A very basic flake";
+  description = "Hercuhub - GitHub bot";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
+    crane.url = "github:ipetkov/crane";
+
+    systems.url = "github:nix-systems/x86_64-linux";
+    flake-utils.url = "github:numtide/flake-utils";
+    flake-utils.inputs.systems.follows = "systems";
   };
 
-  outputs = { self, nixpkgs }: {
+  outputs =
+    {
+      self,
+      nixpkgs,
+      crane,
+      flake-utils,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
 
-    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
+        craneLib = crane.mkLib pkgs;
 
-    packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
+        # Common arguments can be set here to avoid repeating them later
+        # Note: changes here will rebuild all dependency crates
+        commonArgs = {
+          src = craneLib.cleanCargoSource ./.;
+          strictDeps = true;
 
-  };
+          buildInputs =
+            [
+              # Add additional build inputs here
+            ]
+            ++ pkgs.lib.optionals pkgs.stdenv.isDarwin [
+              # Additional darwin specific inputs can be set here
+              pkgs.libiconv
+            ];
+        };
+
+        hercuhub = craneLib.buildPackage (
+          commonArgs
+          // {
+            cargoArtifacts = craneLib.buildDepsOnly commonArgs;
+
+            # Additional environment variables or build phases/hooks can be set
+            # here *without* rebuilding all dependency crates
+            # MY_CUSTOM_VAR = "some value";
+          }
+        );
+      in
+        {
+          checks = {
+            inherit hercuhub;
+          };
+
+          packages.default = hercuhub;
+
+          apps.default = flake-utils.lib.mkApp {
+            drv = hercuhub;
+          };
+
+          devShells.default = craneLib.devShell {
+            # Inherit inputs from checks.
+            checks = self.checks.${system};
+
+            # Additional dev-shell environment variables can be set directly
+            # MY_CUSTOM_DEVELOPMENT_VAR = "something else";
+
+            # Extra inputs can be added here; cargo and rustc are provided by default.
+            packages = [
+              # pkgs.ripgrep
+            ];
+          };
+        }
+    );
 }
